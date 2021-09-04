0fabc5c8846b44b047b699e68022e307c7e5e5ed
julia <jal@covis.de>
2021-09-04 13:54:47 +0200

    changed react text

---
+++ src/pages/WelcomePage.js
@@ -0,0 +1,13 @@
+export const WelcomePage = () => {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>
+                   Find your daily route
+                </h1>
+                <p>Walking is healthy!</p>
+            </header>
+        </div>
+    );
+}
+

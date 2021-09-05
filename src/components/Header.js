175b7c6995d8c37e76b574536187b0327765b648
julia <jal@covis.de>
2021-09-05 10:16:33 +0200

    changed header image

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import park from '../assets/park.jpg'
 
 export const Header = () => {
     return (
-        <div  className="h-40 flex bg-park justify-center items-center w-full" style={{backgroundRepeat: 'no-repeat', backgroundSize: 'cover'}}>
+        <div  className=" flex bg-park justify-center items-center w-screen" style={{backgroundRepeat: 'no-repeat', backgroundSize: '100vw', height: '425px'}}>
             <div className="bg-white p-4 rounded-2xl text-2xl">
                     Find your daily route
             </div>

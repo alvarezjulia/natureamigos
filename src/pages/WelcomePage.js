af4ecdebed17d30e81dd24c44219a22ca66191c6
Nancy Nyambura <nancynyambura@Nancys-MacBook-Pro.local>
2021-09-05 10:49:22 +0200

    added images

--- src/pages/WelcomePage.js
+++ src/pages/WelcomePage.js
@@ -41,8 +41,16 @@ export const WelcomePage = () => {
             <WalkOptions setCurrentRoute={setCurrentRoute} />
 
             <Articles />
-
-
+            <div className = "BlogContainer">
+                <div className = "ArticleBackground1">
+                    <div className = "ArticlePic1"/>
+                    <div className = "ArticleTitle1"> Asana Yoga in the Park</div>
+                    <div classNAme = "ArticleText1"> Every Thursday at 7 p.m. Lisa guides you through a Bikram Yoga session in this park for an hour. 
+                    Skilful and personable, with a microphone and music. Meanwhile 100 yogis like to gather with their mats in this spacious area 
+                    in the park which makes the experience even more impressive.</div>
+                </div>
+            </div>
+            
             <footer>
                 <p>I am footer</p>
             </footer>

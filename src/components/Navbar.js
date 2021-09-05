65b83331aefcf96ccece1210134e6f56e406612b
mingchenglee <mingchenglee9@gmail.com>
2021-09-05 11:39:32 +0200

    setting up the links on the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,8 +2,8 @@
 export const Navbar = () => {
     return (
         <nav className="Navbar">
-            <a href="#header" className="Nav">Parks</a>
-            <a href="" className="Nav">News</a>
+            <a href="#map" className="Nav">Parks</a>
+            <a href="#news" className="Nav">News</a>
             <a href="" className="Nav">Group Up</a>
             <a href="" className="Nav">Impressum</a>
         </nav>
--- src/pages/WelcomePage.js
+++ src/pages/WelcomePage.js
@@ -16,8 +16,8 @@ export const WelcomePage = () => {
             <header id="header" className="App-header">
                 <Navbar />
             </header>
-            <Header/>
-            
+            <Header />
+
 
             <form className="form">
                 <input type="text" placeholder="Where do you live?"></input>
@@ -30,30 +30,30 @@ export const WelcomePage = () => {
             <div></div>
             <div></div>
 
-            <div className="flex flex-row justify-center px-4 w-full">
+            <div className="flex flex-row justify-center px-4 w-full" id="map" >
                 <RouteMap currentRoute={currentRoute} />
-                <WalkOptions setCurrentRoute={setCurrentRoute}  currentRoute={currentRoute} />
+                <WalkOptions setCurrentRoute={setCurrentRoute} currentRoute={currentRoute} />
             </div>
 
 
-            <div className="w-full flex justify-items-center items-center my-6 py-6 flex-col" style={{backgroundColor: '#F8FBF6', height: '700px'}}>
-               <h1 className="text-4xl m-2"> Air pollution map in Düsseldorf</h1>
-                <img src={pollutionMap} alt="pollution map" width="800px"/>
+            <div className="w-full flex justify-items-center items-center my-6 py-6 flex-col" style={{ backgroundColor: '#F8FBF6', height: '700px' }}>
+                <h1 className="text-4xl m-2"> Air pollution map in Düsseldorf</h1>
+                <img src={pollutionMap} alt="pollution map" width="800px" />
             </div>
             <Articles />
-            <div className = "BlogContainer">
-                <div className = "ArticleBackground1">
-                    <div className = "ArticlePic1"/>
-                    <div className = "ArticleTitle1"> Asana Yoga in the Park</div>
-                    <div classNAme = "ArticleText1"> Every Thursday at 7 p.m. Lisa guides you through a Bikram Yoga session in this park for an hour. 
-                    Skilful and personable, with a microphone and music. Meanwhile 100 yogis like to gather with their mats in this spacious area 
-                    in the park which makes the experience even more impressive.</div>
+            <div className="BlogContainer" id="news">
+                <div className="ArticleBackground1">
+                    <div className="ArticlePic1" />
+                    <div className="ArticleTitle1"> Asana Yoga in the Park</div>
+                    <div classNAme="ArticleText1"> Every Thursday at 7 p.m. Lisa guides you through a Bikram Yoga session in this park for an hour.
+                        Skilful and personable, with a microphone and music. Meanwhile 100 yogis like to gather with their mats in this spacious area
+                        in the park which makes the experience even more impressive.</div>
                 </div>
             </div>
-            
+
             <button className="Button">Load more</button>
 
-           
+
 
 
             <footer>
